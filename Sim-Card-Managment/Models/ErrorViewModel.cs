1948269efe2867dfab7181c6cc32aaa60f6d2afa
sd <sd@COM168-LAB3>
2026-06-22 13:42:18 +0300

    create project

---
+++ Sim-Card-Managment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sim_Card_Managment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
