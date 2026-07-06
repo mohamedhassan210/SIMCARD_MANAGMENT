e2b4b5c11d3dce8ddd17334a52c0bf4de38a1fc9
m.eldokany <m.eldokany@LAB3-MAST-186>
2026-07-06 10:16:02 +0300

    ready to pull

--- Sim-Card-Managment/Controllers/Accountcontroller.cs
+++ Sim-Card-Managment/Controllers/Accountcontroller.cs
@@ -7,6 +7,7 @@ using Sim_Card_Managment.Viewmodel;
 using Sim_Card_Managment.Models;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Sim_Card_Managment.Controllers
 {
