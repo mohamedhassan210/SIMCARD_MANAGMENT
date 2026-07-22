8b9911142c9ebd30db939628c2aa4d4f8e09b887
sd <sd@COMLAB2-PC34>
2026-07-22 10:44:46 +0300

    handle account view and layout

--- SIMCARD.Test/obj/Release/net8.0/SIMCARD.Test.AssemblyInfo.cs
+++ SIMCARD.Test/obj/Release/net8.0/SIMCARD.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4329ffd5b6f1ea99b52b52a4a842a341bf641b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37ee3f19375160026a0dce2ad2db897fc4cf041")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
