994ed54226e46a14fa4ed39214f042c3c5ac7965
sd <sd@COM171-LAB3>
2026-07-22 10:26:53 +0300

    ready

--- SIMCARD.Test/obj/Debug/net8.0/SIMCARD.Test.AssemblyInfo.cs
+++ SIMCARD.Test/obj/Debug/net8.0/SIMCARD.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39901cd4201b2ceb41f427616a213f1ed835bd77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ceaeec2b4921a45bbaeef649fdde6fe80ae45a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
