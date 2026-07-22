87dba70f8e47f354b481a2644148a88d3ada98e9
sd <sd@COMLAB2-PC34>
2026-07-22 13:00:05 +0300

    handle Document views

--- SIMCARD.Test/obj/Debug/net8.0/SIMCARD.Test.AssemblyInfo.cs
+++ SIMCARD.Test/obj/Debug/net8.0/SIMCARD.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e6515ab3934a535d5e483e8bcdd6a89deb7049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed99ce21658eaecf9c2c2f62c97e05bbe9a3d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
