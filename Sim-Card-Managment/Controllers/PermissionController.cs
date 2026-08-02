dac817cbf92d208d1a115da0b06629e41f1d2edf
sd <sd@COM169-LAB3>
2026-08-02 12:31:10 +0200

    added create group ,permissions views

---
+++ Sim-Card-Managment/Controllers/PermissionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sim_Card_Management.Controllers
+{
+    public class PermissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MangePermission() { return View(); } 
+    }
+}
