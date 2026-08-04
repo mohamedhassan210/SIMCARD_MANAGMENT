c75f95f632f545f2e611409eb7b0d35809a95834
Badr <sd@COM167-LAB3>
2026-08-04 09:55:17 +0300

    fix: restrict Group and Permission controllers to authorized users only

--- Sim-Card-Managment/Controllers/GroupController.cs
+++ Sim-Card-Managment/Controllers/GroupController.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 
 namespace Sim_Card_Managment.Controllers
 {
+    [RequirePermission]
     public class GroupController : Controller
     {
         private readonly IGroupRepo _groups;
