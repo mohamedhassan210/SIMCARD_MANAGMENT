8ae59a3bfd7a124cb9b527654f9bc6f165cedc55
Badr <sd@COM167-LAB3>
2026-08-11 13:11:28 +0300

    edit-DeviceAction

---
+++ Sim-Card-Managment/Migrations/20260811101018_edit-DeviceAction.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sim_Card_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class editDeviceAction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_Actions_ActionId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actions",
+                table: "Actions");
+
+            migrationBuilder.RenameTable(
+                name: "Actions",
+                newName: "DeviceActions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DeviceActions",
+                table: "DeviceActions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_DeviceActions_ActionId",
+                table: "Subscriptions",
+                column: "ActionId",
+                principalTable: "DeviceActions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_DeviceActions_ActionId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DeviceActions",
+                table: "DeviceActions");
+
+            migrationBuilder.RenameTable(
+                name: "DeviceActions",
+                newName: "Actions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actions",
+                table: "Actions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_Actions_ActionId",
+                table: "Subscriptions",
+                column: "ActionId",
+                principalTable: "Actions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
