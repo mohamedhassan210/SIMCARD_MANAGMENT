b595324e02a15dd02acfd42da70850aedf858f84
0PepsiMan <yosefehab515@gmail.com>
2026-08-21 23:15:16 +0300

    new migration to add IsActive to NonEmployee table

--- SIMCARD.Test/obj/Debug/net8.0/SIMCARD.Test.AssemblyInfo.cs
+++ SIMCARD.Test/obj/Debug/net8.0/SIMCARD.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1700db786b24f13231cb9ac25111952dd1cd84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfbf564e61ac3fe892db19db116e944a5a5b8466")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIMCARD.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
